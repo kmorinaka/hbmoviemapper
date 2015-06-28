ee8b66fdf0ac97602f516ad5432853fbc15839e8
Angela Castanieto <angela@Jeffs-Mac.local>
2015-06-27 19:51:25 -0700

    Single marker showing up on map

--- mapper.js
+++ mapper.js
@@ -1,13 +1,21 @@
 // render google map
-	      function initialize() {
-	        var mapOptions = {
-	          center: { lat: -34.397, lng: 150.644},
-	          zoom: 8
-	        };
-	        var map = new google.maps.Map(document.getElementById('map-canvas'),
-	            mapOptions);
-	      }
-	      google.maps.event.addDomListener(window, 'load', initialize);
+var map;
+function initialize() {
+  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  var mapOptions = {
+    zoom: 4,
+    center: myLatlng
+  }
+   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 	// get value from user input
 		var ref = new Firebase("https://fiery-fire-3549.firebaseio.com/");
@@ -16,8 +24,8 @@
 			$("#title-form").submit(function(evt){
 				evt.preventDefault();
 				var title = $("#title").val();
-				console.log("hi");
 
+// pull in data from firebase.  this loops through each lat long and console logs
 				ref.child("transformed-data")
 				  .orderByChild("movieName")
 					.equalTo(title).on("child_added", function(snap) {
@@ -25,8 +33,45 @@
 						var lat = value.lat;
 						var lon = value.lon;
 						console.log(lat + " "+lon)
-						});
+						var myLatlng = new google.maps.LatLng(lat, lon);
+						// var mapOptions = {
+						//   zoom: 4,
+						//   center: myLatlng
+						// }
 
+						var marker = new google.maps.Marker({
+						    position: myLatlng,
+						    title:"Hello World!"
+						});
+						marker.setMap(map);
+
+// display marker on map for each lat long
+
+
+
+
+
+			// 			var myLatlng = new google.maps.LatLng(lat, lon);
+			// 			var mapOptions = {
+			// 	  zoom: 4,
+			// 	  center: myLatlng
+			// 	}
+			// 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+			//
+			// 	var marker = new google.maps.Marker({
+			// 	    position: myLatlng,
+			// 	    title:"Hello World!"
+			// 	});
+			//
+			//
+			//
+			// 	// To add the marker to the map, call setMap();
+			// 	marker.setMap(map);
+			//
 			});
+		});
 });
+
+// google.maps.event.addDomListener(window, 'load', initialize);
+
 	// Firebase query:
