39721ee233182b2180f5201b223d4890a61f55c5
Tiffany8 <tiffany@TRWilliams.local>
2015-06-28 08:05:45 -0700

    Integrated the draggable map js file and added geofire specific info to the mapper.html and the mapper.js

--- mapper.js
+++ mapper.js
@@ -12,8 +12,22 @@ function initialize() {
 	// create map at the hard-coded lat long for San Francisco
    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-
+   // Create a draggable circle centered on the map
+   var circle = new google.maps.Circle({
+     strokeColor: "#6D3099",
+     strokeOpacity: 0.7,
+     strokeWeight: 1,
+     fillColor: "#B650FF",
+     fillOpacity: 0.35,
+     map: map,
+     center: loc,
+     radius: ((radiusInKm) * 1000),
+     draggable: true
+   });
 }
+
+
+
 
 // render map on div
 google.maps.event.addDomListener(window, 'load', initialize);
