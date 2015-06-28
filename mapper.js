c6a67d877938d783c92c1dcb051fbeff8c393ce1
Angela Castanieto <angela@Jeffs-Mac.local>
2015-06-28 00:33:11 -0700

    Added pop up windows and div to display data

--- mapper.js
+++ mapper.js
@@ -6,7 +6,7 @@ function initialize() {
 
   var myLatlng = new google.maps.LatLng(37.775, -122.4183333);
   var mapOptions = {
-    zoom: 12,
+    zoom: 11,
     center: myLatlng
   }
 	// create map at the hard-coded lat long for San Francisco
@@ -26,13 +26,16 @@ google.maps.event.addDomListener(window, 'load', initialize);
 	}
 
 		var ref = new Firebase("https://fiery-fire-3549.firebaseio.com/");
-		$(document).ready(function() {
+
+    $(document).ready(function() {
 			// get title from user input
 			$("#title-form").submit(function(evt){
 				evt.preventDefault();
 				removeMarkers();
-
+        $("#movie-info").empty();
 				var title = $("#title").val(); // pull in movie title
+
+
 
 // The following loops through all movie objects with the specified movie title in firebase
 // and makes markers at each lat lon and drops them onto the map
@@ -42,18 +45,34 @@ google.maps.event.addDomListener(window, 'load', initialize);
 						var value = snap.val();
 						var lat = value.lat;
 						var lon = value.lon;
-						// console.log(lat + " "+lon)
+						console.log(lat + " "+lon)
 						var myLatlng = new google.maps.LatLng(lat, lon);
 
 						    marker = new google.maps.Marker({
 								position: myLatlng,
 								map: map,
+                clickable: true,
 								title: lat +" " + lon
 						});
+
+            // location name pop up when click on marker
+
+            marker.info = new google.maps.InfoWindow({
+              content: value.locationName
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+              this.info.open(map, this);
+            });
+
+
 						// push markers into array so can clear them later
 						markers.push(marker);
-
+// add movie info to div...another query outside for loop?
+            $("#movie-info").html("<li><ul>"+title
+            +"</ul><ul>"+value.year
+            +"</ul><ul>"+value.director+"</ul></li>");
+      		});
 
 			});
-		});
+
 });
