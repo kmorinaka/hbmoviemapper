f39e77d47b89b99b95f8062111b4edb678c1dfc1
Angela Castanieto <angela@Jeffs-Mac.local>
2015-06-27 20:57:50 -0700

    Added comments explaining functionality

--- mapper.js
+++ mapper.js
@@ -3,42 +3,39 @@ var map;
 var markers = [];
 
 function initialize() {
+
   var myLatlng = new google.maps.LatLng(37.775, -122.4183333);
   var mapOptions = {
     zoom: 12,
     center: myLatlng
   }
+	// create map at the hard-coded lat long for San Francisco
    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 
 }
-
+// render map on div
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
-
-
-	// get value from user input
-
+	// clear markers from map
 	function removeMarkers(){
-for(i=0; i<markers.length; i++){
-	markers[i].setMap(null);
-}
-}
+		for(i=0; i<markers.length; i++){
+			markers[i].setMap(null);
+		}
+	}
 
 		var ref = new Firebase("https://fiery-fire-3549.firebaseio.com/");
 		$(document).ready(function() {
-
+			// get title from user input
 			$("#title-form").submit(function(evt){
 				evt.preventDefault();
-
 				removeMarkers();
 
+				var title = $("#title").val(); // pull in movie title
 
-
-				var title = $("#title").val();
-
-// pull in data from firebase.  this loops through each lat long and console logs
+// The following loops through all movie objects with the specified movie title in firebase
+// and makes markers at each lat lon and drops them onto the map
 				ref.child("transformed-data")
 				  .orderByChild("movieName")
 					.equalTo(title).on("child_added", function(snap) {
@@ -53,42 +50,10 @@ for(i=0; i<markers.length; i++){
 								map: map,
 								title: lat +" " + lon
 						});
-
+						// push markers into array so can clear them later
 						markers.push(marker);
-						console.log(markers);
 
 
-
-
-
-
-// display marker on map for each lat long
-
-
-
-
-
-			// 			var myLatlng = new google.maps.LatLng(lat, lon);
-			// 			var mapOptions = {
-			// 	  zoom: 4,
-			// 	  center: myLatlng
-			// 	}
-			// 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-			//
-			// 	var marker = new google.maps.Marker({
-			// 	    position: myLatlng,
-			// 	    title:"Hello World!"
-			// 	});
-			//
-			//
-			//
-			// 	// To add the marker to the map, call setMap();
-			// 	marker.setMap(map);
-			//
 			});
 		});
 });
-
-// google.maps.event.addDomListener(window, 'load', initialize);
-
-	// Firebase query:
